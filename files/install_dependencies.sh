e6262055f373ad62a6bc2808f17e7b691408e448
Danny Su <contact@dannysu.com>
2016-03-28 12:53:42 -0700

    Update electron version to 0.37.3

--- files/install_dependencies.sh
+++ files/install_dependencies.sh
@@ -11,4 +11,4 @@ $minimal_apt_get_install libgtk2.0-0 libgconf2-4 libnss3 libasound2 libxtst6 xvf
 # Install dependencies needed by 0.36.5+
 $minimal_apt_get_install libnotify-dev dbus-x11
 
-npm install -g electron-prebuilt@0.36.5
+npm install -g electron-prebuilt@0.37.3
